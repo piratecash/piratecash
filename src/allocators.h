adce3c7952adace2ea9de7e09dc8e6d44e4d2f53
Dmitriy Korniychuk <dmitriy@korniychuk.org.ua>
2018-11-27 21:24:12 +0200

    [fix] Windows 7 - Assertion failed #5

--- src/allocators.h
+++ src/allocators.h
@@ -35,11 +35,6 @@ public:
         page_mask = ~(page_size - 1);
     }
 
-    ~LockedPageManagerBase()
-    {
-        assert(this->GetLockedPageCount() == 0);
-    }
-
     // For all pages in affected range, increase lock count
     void LockRange(void *p, size_t size)
     {
