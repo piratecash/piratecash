0807235fa3c190eddcfd951a309e7117420f08df
Dmitriy Korniychuk <dmitriy@korniychuk.org.ua>
2019-01-30 20:52:53 +0200

    remove

--- src/net.cpp
+++ src/net.cpp
@@ -710,9 +710,6 @@ bool CNode::ReceiveMsgBytes(const char *pch, unsigned int nBytes)
 
         pch += handled;
         nBytes -= handled;
-
-        if (msg.complete())
-            msg.nTime = GetTimeMicros();
     }
 
     return true;
