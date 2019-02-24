9a564e7334ad8f3efcd2618980ebea3d3c89434d
Dmitriy Korniychuk <dmitriy@korniychuk.org.ua>
2019-02-24 10:16:08 +0200

    1.0.9

--- src/version.cpp
+++ src/version.cpp
@@ -31,7 +31,7 @@ const std::string CLIENT_NAME("PirateCash");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID "60014"
+#    define GIT_COMMIT_ID "60015"
 #endif
 
 #define BUILD_DESC_FROM_COMMIT(maj,min,rev,build,commit) \
