296817e2768cde0aee0c600c9ff633a3eb9cf2fa
The Pirate Digger <digger@piratecash.net>
2019-04-09 10:56:15 +0100

    Fix build piratecashd

--- src/bitcoind.cpp
+++ src/bitcoind.cpp
@@ -3,9 +3,13 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include "clientversion.h"
 #include "rpcserver.h"
 #include "rpcclient.h"
 #include "init.h"
+#include "main.h"
+#include "ui_interface.h"
+#include "util.h"
 #include <boost/algorithm/string/predicate.hpp>
 #include <boost/thread.hpp>
 
