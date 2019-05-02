1994f5f74b7f49036342257ce304e02529339664
Dmitriy Korniychuk <dmitriy@korniychuk.org.ua>
2019-05-02 16:01:44 +0300

    Temporary unlock MN coins locked by -mnconflock on masternode start-lias/many and immediately lock them back right after finding all available coins

--- src/activemasternode.cpp
+++ src/activemasternode.cpp
@@ -5,6 +5,7 @@
 #include "protocol.h"
 #include "activemasternode.h"
 #include "masternodeman.h"
+#include "masternodeconfig.h"
 #include <boost/lexical_cast.hpp>
 #include "clientversion.h"
 
@@ -429,9 +430,27 @@ vector<COutput> CActiveMasternode::SelectCoinsMasternode()
 {
     vector<COutput> vCoins;
     vector<COutput> filteredCoins;
+    vector<COutPoint> confLockedCoins;
+
+    // Temporary unlock MN coins from masternode.conf
+    if(GetBoolArg("-mnconflock", true)) {
+        uint256 mnTxHash;
+        BOOST_FOREACH(CMasternodeConfig::CMasternodeEntry mne, masternodeConfig.getEntries()) {
+            mnTxHash.SetHex(mne.getTxHash());
+            COutPoint outpoint = COutPoint(mnTxHash, boost::lexical_cast<unsigned int>(mne.getOutputIndex()));
+            confLockedCoins.push_back(outpoint);
+            pwalletMain->UnlockCoin(outpoint);
+        }
+    }
 
     // Retrieve all possible outputs
     pwalletMain->AvailableCoinsMN(vCoins);
+
+    // Lock MN coins from masternode.conf back if they where temporary unlocked
+    if(!confLockedCoins.empty()) {
+        BOOST_FOREACH(COutPoint outpoint, confLockedCoins)
+                pwalletMain->LockCoin(outpoint);
+    }
 
     // Filter
     BOOST_FOREACH(const COutput& out, vCoins)
