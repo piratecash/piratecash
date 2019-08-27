097d959933e4643a0fe051bd094d7fc92bbdf232
Dmitriy Korniychuk <dmitriy@korniychuk.org.ua>
2019-08-27 14:34:01 +0300

    Bump to 0.11.5

--- src/version.h
+++ src/version.h
@@ -30,7 +30,7 @@ static const int DATABASE_VERSION = 70509;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60019;
+static const int PROTOCOL_VERSION = 60020;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -52,7 +52,7 @@ static const int CADDR_TIME_VERSION = 31402;
 
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 0;
-static const int NOBLKS_VERSION_END = 60010;
+static const int NOBLKS_VERSION_END = 60018;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
