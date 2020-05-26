9732b905017aa321dbd82887f1521120206cda08
PirateDigger <piratecashnet@gmail.com>
2020-05-26 20:02:58 +0300

    Bump 11.5.8

--- src/version.h
+++ src/version.h
@@ -30,24 +30,24 @@ static const int DATABASE_VERSION = 70509;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60021;
+static const int PROTOCOL_VERSION = 60022;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 60020;
+static const int MIN_PEER_PROTO_VERSION = 60021;
 
 // minimum peer version accepted by DarkSendPool
-static const int MIN_POOL_PEER_PROTO_VERSION = 60020;
+static const int MIN_POOL_PEER_PROTO_VERSION = 60021;
 
-static const int MIN_INSTANTX_PROTO_VERSION = 60020;
+static const int MIN_INSTANTX_PROTO_VERSION = 60021;
 
 // minimum peer version that can receive masternode payments
-static const int MIN_MASTERNODE_PAYMENT_PROTO = 60020;
+static const int MIN_MASTERNODE_PAYMENT_PROTO = 60021;
 
 //! minimum peer version for masternode winner broadcasts
-static const int MIN_MNW_PEER_PROTO_VERSION = 60020;
+static const int MIN_MNW_PEER_PROTO_VERSION = 60021;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
@@ -55,7 +55,7 @@ static const int CADDR_TIME_VERSION = 31402;
 
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 0;
-static const int NOBLKS_VERSION_END = 60020;
+static const int NOBLKS_VERSION_END = 60021;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
