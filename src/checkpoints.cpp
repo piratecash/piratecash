755e5d5aec92014d5c534db1a45a8458918403c1
The Pirate Digger <piratecashnet@gmail.com>
2022-06-30 16:46:46 +0300

    Add checkpoint on 928967 (network split, disable old versions)

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -42,6 +42,7 @@ namespace Checkpoints
         (687687, uint256("0x981a2ee7a4c74f34689ec6de92bb3cbaf00aaf231a9b78ea97bb21a2e8bb3a42"))
         (741417, uint256("0x7440bc3d9dcf3b9f3208267e54eb0def7976b2ea0ffa656c0a54ac14614d04a3"))
         (912219, uint256("0xde67bb79a990b039d3ede324f8a58dc0ea6f1393df8d75fbd72ea581ceb46280"))
+        (928967, uint256("0x895992d6f7b73606b7fbef9d155a4ba98911cdf2b73b07a018e6903d08da11c5"))
     ;
 
     static ListBannedBlocks listBanned =
--- src/version.h
+++ src/version.h
@@ -31,7 +31,7 @@ static const int DATABASE_VERSION = 70509;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60026;
+static const int PROTOCOL_VERSION = 60027;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -56,7 +56,7 @@ static const int CADDR_TIME_VERSION = 31402;
 
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 0;
-static const int NOBLKS_VERSION_END = 60021;
+static const int NOBLKS_VERSION_END = 60025;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
