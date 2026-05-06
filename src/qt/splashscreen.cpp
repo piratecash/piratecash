d6e732f7e7c9bb4c6f0c02317bfa6d88b2c7d76e
Dmitriy Korniychuk <dmitriy@korniychuk.org.ua>
2026-05-06 22:42:42 +0300

    qt: brand application icons and installer artwork

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -66,7 +66,7 @@ SplashScreen::SplashScreen(const NetworkStyle *networkStyle) :
 
     // Adjust logo color based on the current theme
     QImage imgLogo = pixmapLogo.toImage().convertToFormat(QImage::Format_ARGB32);
-    QColor logoColor = GUIUtil::getThemedQColor(GUIUtil::ThemedColor::BLUE);
+    QColor logoColor = QColor(147, 147, 147, 0);
     for (int x = 0; x < imgLogo.width(); ++x) {
         for (int y = 0; y < imgLogo.height(); ++y) {
             const QRgb rgb = imgLogo.pixel(x, y);
