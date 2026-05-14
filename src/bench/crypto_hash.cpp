139a6e93d832ace73c4f74813f0c4aee67c6a332
Dmitriy Korniychuk <dmitriy@korniychuk.org.ua>
2026-05-14 16:47:21 +0300

    crypto: replace X11 hashing with PirateCash scrypt

--- src/bench/crypto_hash.cpp
+++ src/bench/crypto_hash.cpp
@@ -13,7 +13,6 @@
 #include <crypto/sha512.h>
 #include <crypto/siphash.h>
 #include <hash.h>
-#include <hash_x11.h>
 #include <random.h>
 #include <uint256.h>
 
@@ -76,15 +75,6 @@ static void HASH_1MB_SHA3_256(benchmark::Bench& bench)
     });
 }
 
-static void HASH_1MB_X11(benchmark::Bench& bench)
-{
-    uint256 hash;
-    std::vector<uint8_t> in(BUFFER_SIZE,0);
-    bench.batch(in.size()).unit("byte").minEpochIterations(10).run([&] {
-        hash = HashX11(in.begin(), in.end());
-    });
-}
-
 /* Hash different number of bytes via DSHA256 */
 
 static void HASH_DSHA256_0032b_single(benchmark::Bench& bench)
@@ -138,62 +128,6 @@ static void HASH_DSHA256_2048b_single(benchmark::Bench& bench)
     std::vector<uint8_t> in(2048,0);
     bench.minEpochIterations(100000).run([&] {
         CHash256().Write(in).Finalize(hash);
-    });
-}
-
-/* Hash different number of bytes via X11 */
-
-static void HASH_X11_0032b_single(benchmark::Bench& bench)
-{
-    uint256 hash;
-    std::vector<uint8_t> in(32,0);
-    bench.minEpochIterations(10000).run([&] {
-        hash = HashX11(in.begin(), in.end());
-    });
-}
-
-static void HASH_X11_0080b_single(benchmark::Bench& bench)
-{
-    uint256 hash;
-    std::vector<uint8_t> in(80,0);
-    bench.minEpochIterations(10000).run([&] {
-        hash = HashX11(in.begin(), in.end());
-    });
-}
-
-static void HASH_X11_0128b_single(benchmark::Bench& bench)
-{
-    uint256 hash;
-    std::vector<uint8_t> in(128,0);
-    bench.minEpochIterations(10000).run([&] {
-        hash = HashX11(in.begin(), in.end());
-    });
-}
-
-static void HASH_X11_0512b_single(benchmark::Bench& bench)
-{
-    uint256 hash;
-    std::vector<uint8_t> in(512,0);
-    bench.minEpochIterations(10000).run([&] {
-        hash = HashX11(in.begin(), in.end());
-    });
-}
-
-static void HASH_X11_1024b_single(benchmark::Bench& bench)
-{
-    uint256 hash;
-    std::vector<uint8_t> in(1024,0);
-    bench.minEpochIterations(10000).run([&] {
-        hash = HashX11(in.begin(), in.end());
-    });
-}
-
-static void HASH_X11_2048b_single(benchmark::Bench& bench)
-{
-    uint256 hash;
-    std::vector<uint8_t> in(2048,0);
-    bench.minEpochIterations(10000).run([&] {
-        hash = HashX11(in.begin(), in.end());
     });
 }
 
@@ -296,7 +230,6 @@ BENCHMARK(HASH_1MB_SHA1);
 BENCHMARK(HASH_1MB_SHA256);
 BENCHMARK(HASH_1MB_SHA512);
 BENCHMARK(HASH_1MB_SHA3_256);
-BENCHMARK(HASH_1MB_X11);
 
 BENCHMARK(HASH_DSHA256_0032b_single);
 BENCHMARK(HASH_DSHA256_0080b_single);
@@ -304,12 +237,6 @@ BENCHMARK(HASH_DSHA256_0128b_single);
 BENCHMARK(HASH_DSHA256_0512b_single);
 BENCHMARK(HASH_DSHA256_1024b_single);
 BENCHMARK(HASH_DSHA256_2048b_single);
-BENCHMARK(HASH_X11_0032b_single);
-BENCHMARK(HASH_X11_0080b_single);
-BENCHMARK(HASH_X11_0128b_single);
-BENCHMARK(HASH_X11_0512b_single);
-BENCHMARK(HASH_X11_1024b_single);
-BENCHMARK(HASH_X11_2048b_single);
 
 BENCHMARK(HASH_SHA256_32b);
 BENCHMARK(HASH_SipHash_32b);
--- src/hash.h
+++ src/hash.h
@@ -7,6 +7,7 @@
 #ifndef BITCOIN_HASH_H
 #define BITCOIN_HASH_H
 
+#include <crypto/scrypt.h>
 #include <attributes.h>
 #include <crypto/common.h>
 #include <crypto/ripemd160.h>
