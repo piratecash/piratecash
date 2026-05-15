3e7bd75556598315042e495af0071d439c80bb5d
Dmitriy Korniychuk <dmitriy@korniychuk.org.ua>
2026-05-15 08:46:35 +0300

    chain: persist PirateCash PoS index metadata

--- src/txdb.cpp
+++ src/txdb.cpp
@@ -446,10 +446,18 @@ bool CBlockTreeDB::LoadBlockIndexGuts(const Consensus::Params& consensusParams, 
                 pindexNew->nTime          = diskindex.nTime;
                 pindexNew->nBits          = diskindex.nBits;
                 pindexNew->nNonce         = diskindex.nNonce;
+                pindexNew->posStakeHash   = diskindex.posStakeHash;
+                pindexNew->posStakeN      = diskindex.posStakeN;
+                pindexNew->posBlockSig    = diskindex.posBlockSig;
                 pindexNew->nStatus        = diskindex.nStatus;
                 pindexNew->nTx            = diskindex.nTx;
 
-                if (!CheckProofOfWork(pindexNew->GetBlockHash(), pindexNew->nBits, consensusParams)) {
+                // PirateCash related block index fields
+                pindexNew->nFlags         = diskindex.nFlags;
+
+                if (pindexNew->IsProofOfWork() &&
+                    pindexNew->GetBlockHash() != consensusParams.hashGenesisBlock &&
+                    !CheckProofOfWork(pindexNew->GetBlockHeader().GetPoWHash(), pindexNew->nBits, consensusParams)) {
                     return error("%s: CheckProofOfWork failed: %s", __func__, pindexNew->ToString());
                 }
 
