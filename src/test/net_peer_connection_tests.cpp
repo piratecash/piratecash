3ff09fd48ff5678b56c74ee7b17654a60911076e
Dmitriy Korniychuk <dmitriy@korniychuk.org.ua>
2026-07-18 15:31:41 +0300

    test: use chain default port in net_peer_connection_tests

--- src/test/net_peer_connection_tests.cpp
+++ src/test/net_peer_connection_tests.cpp
@@ -89,7 +89,7 @@ BOOST_AUTO_TEST_CASE(test_addnode_getaddednodeinfo_and_connection_detection)
 
     // Connect a localhost peer.
     {
-        ASSERT_DEBUG_LOG("Added connection to 127.0.0.1:9999 peer=1");
+        ASSERT_DEBUG_LOG(strprintf("Added connection to 127.0.0.1:%d peer=1", chainparams.GetDefaultPort()));
         AddPeer(id, nodes, *peerman, *connman, ConnectionType::MANUAL, /*onion_peer=*/false, /*address=*/"127.0.0.1");
         BOOST_REQUIRE(nodes.back() != nullptr);
     }
@@ -98,7 +98,7 @@ BOOST_AUTO_TEST_CASE(test_addnode_getaddednodeinfo_and_connection_detection)
     // address that resolves to multiple IPs, including that of the connected peer.
     // The connection attempt should consistently fail due to the check in ConnectNode().
     for (int i = 0; i < 10; ++i) {
-        ASSERT_DEBUG_LOG("Not opening a connection to localhost, already connected to 127.0.0.1:9999");
+        ASSERT_DEBUG_LOG(strprintf("Not opening a connection to localhost, already connected to 127.0.0.1:%d", chainparams.GetDefaultPort()));
         BOOST_CHECK(!connman->ConnectNodePublic(*peerman, "localhost", ConnectionType::MANUAL));
     }
 
