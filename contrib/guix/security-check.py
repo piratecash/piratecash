64c5235ef2021b8424648aca7f8b38ba817de3c3
Dmitriy Korniychuk <dmitriy@korniychuk.org.ua>
2026-07-19 13:41:20 +0300

    guix: fix FORTIFY check for piratecash-util

--- contrib/guix/security-check.py
+++ contrib/guix/security-check.py
@@ -123,8 +123,8 @@ def check_ELF_CONTROL_FLOW(binary) -> bool:
     return False
 
 def check_ELF_FORTIFY(binary) -> bool:
-    # dash-util does not currently contain any fortified functions
-    if 'Dash Core dash-util utility version ' in binary.strings:
+    # piratecash-util does not currently contain any fortified functions
+    if 'PirateCash Core piratecash-util utility version ' in binary.strings:
         return True
 
     chk_funcs = set()
